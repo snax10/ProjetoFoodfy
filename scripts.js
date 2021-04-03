4627b6ef98b38400c7aa208678eafd32da5ecc26
carlos_santos0702@hotmail.com <carlos_santos0702@hotmail.com>
2021-04-03 12:58:42 -0300

    Projeto Foodfy

---
+++ scripts.js
@@ -0,0 +1,21 @@
+const modalOverlay = document.querySelector(".modal-overlay");
+const cards = document.querySelectorAll('.receita');
+
+for (let receita of cards) {
+    receita.addEventListener("click", function () {
+        modalOverlay.classList.add('active')
+
+        const imagemId = receita.getAttribute("id")
+        const modalTitulo = receita.querySelector("h1").textContent
+        const modalAutor = receita.querySelector("p").textContent
+
+        modalOverlay.querySelector("img").src = `/assets/${imagemId}`
+        modalOverlay.querySelector("#modal-titulo").textContent = modalTitulo
+        modalOverlay.querySelector("#modal-autor").textContent = modalAutor
+
+    })
+}
+
+closeModal = document.querySelector(".close-modal").addEventListener("click", function () {
+    modalOverlay.classList.remove("active")
+})
